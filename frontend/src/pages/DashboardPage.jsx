7caf917f133ea0e38b82528d29081d3f279395bd
YanZink <119172259+YanZink@users.noreply.github.com>
2025-10-19 23:49:02 +0300

    Update DashboardPage.jsx

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -168,7 +168,6 @@ const DashboardPage = () => {
                 <Button
                   variant="telegramButton"
                   size="large"
-                  variant="telegramButton"
                   onClick={() =>
                     window.open(
                       process.env.REACT_APP_TELEGRAM_BOT_URL,
